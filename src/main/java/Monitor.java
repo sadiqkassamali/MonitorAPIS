1bbf4f0c6151c40930bb1af0ade8cdaca7977850
sadiq kassamali <Sadiq kassamali@gmail.com>
2023-09-14 04:02:21 -0500

    ini Ui commit

--- src/main/java/Monitor.java
+++ src/main/java/Monitor.java
@@ -1,6 +1,5 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @PropertySource("classpath:application.properties")
 @EnableScheduling
-public class Monitor extends SpringBootServletInitializer {
+public class Monitor {
     public static void main(String[] args) {
         SpringApplication.run(Monitor.class, args);
     }
